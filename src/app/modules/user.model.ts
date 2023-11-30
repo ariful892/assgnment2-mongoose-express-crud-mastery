e94f4e6f64edf50be3001bd090898880f7af8502
ariful892 <ariful00892@gmail.com>
2023-12-01 01:20:16 +0600

    user interface and schema created

---
+++ src/app/modules/user.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+import { Address, FullName, Orders, User } from './user/user.interface';
+
+const userNameSchema = new Schema<FullName>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+const addressSchema = new Schema<Address>({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+const ordersSchema = new Schema<Orders>({
+  orderId: { type: Number, required: true },
+  product: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+});
+
+const userSchema = new Schema<User>({
+  userId: { type: Number, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: userNameSchema,
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: { type: [String], required: true },
+  address: addressSchema,
+  orders: [ordersSchema],
+});
+
+const User = model<User>('User', userSchema);
